e3591d6f5596c42e48093df801739867c526df14
norstc <norstc@gmail.com>
2017-05-08 11:32:47 +0800

    selenium2demo initial

---
+++ SeleniumWebdriverDemo/src/main/java/com/norstc/seleniumwebdriverdemo/Selenium2Demo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.norstc.seleniumwebdriverdemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium2Demo {
+    public static void main(String[] args){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.bing.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("新四经");
+        element.submit();
+        
+        System.out.println("Page title is: " + driver.getTitle());
+     
+        
+    }
+}
