f82f0d9e382155db7110f99261646aed6a5a6e51
norstc <norstc@gmail.com>
2017-06-01 17:23:49 +0800

    补齐导航页面

---
+++ ambleinfo/src/main/java/com/norstc/ambleinfo/entity/Customer.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.norstc.ambleinfo.entity;
+
+/**
+ *
+ * @author zhangjh
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private Integer customerId; 
+ 
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Integer getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+}
---
+++ ambleinfo/src/main/java/com/norstc/ambleinfo/service/CustomerBean.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.norstc.ambleinfo.service;
+
+import com.norstc.ambleinfo.entity.Customer;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author zhangjh
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean {
+
+    private List<Customer> customers;
+ 
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+    /**
+     * Creates a new instance of CustomerBean
+     */
+    public CustomerBean() {
+    }
+    @PostConstruct
+    public void setup()  {
+        List<Customer> customers = new ArrayList<Customer>();
+ 
+        Customer customer1 = new Customer();
+        customer1.setFirstName("John");
+        customer1.setLastName("Doe");
+        customer1.setCustomerId(123456);
+ 
+        customers.add(customer1);
+ 
+        Customer customer2 = new Customer();
+        customer2.setFirstName("Adam");
+        customer2.setLastName("Scott");
+        customer2.setCustomerId(98765);
+ 
+        customers.add(customer2);
+ 
+        Customer customer3 = new Customer();
+        customer3.setFirstName("Jane");
+        customer3.setLastName("Doe");
+        customer3.setCustomerId(65432);
+ 
+        customers.add(customer3);
+        this.customers = customers;
+    }
+}
